581c367f010190abff7f55c22c3069eb0bd66c22
Alex <webmaster@papagai.de>
2022-02-11 14:44:13 +0100

    create

--- script.js
+++ script.js
@@ -1,60 +1,50 @@
 
-/*const btn = document.getElementById('btn')
-
-console.log(btn)
-
-
-btn.onclick =function () { 
-  alert('Клик по кнопке')
- }
-
-console.dir(btn)
-
-//------------------------------------
-*/
 const square = document.getElementById('square')
 
-console.log(square)
-
-//const logger = function () {
- // alert('Клик по кнопке')
-//}
 
 
-/*console.dir(square);
+const logger = function (event) {
+  console.log(event.type) // описание (type) - типа event - события
 
-const logger = function () {
-  console.log('Клик по кнопке')// слева в консоле счётчик , сколько раз отработал Клик
-}
-/*const logger1 = function () {
-  console.log('Клик по кнопке !!!!!!!!!!!')// слева в консоле счётчик , сколько раз отработал Клик
-}
-
-square.onclick = function(){
-  logger1()
-  logger()
-}  // через функцию можно задавать две функциие, иначе последняя затирает предыдущую
-
-square.onclick = null // зартереть функцию
-*/
-
-//square.addEventListener('click', logger) // добавляет функционал к объекту дополнительно
-
-/*square.addEventListener('click', function () {
-  console.log('Клик по кнопке !!!!!!!!!!')})
-
-square.removeEventListener('click', logger) // удаляет функционал , анонимные функции не удаляются
-*/
-
-let counter = 0
-const logger = function () {
-  counter++
-  if (counter===3) {
-    square.removeEventListener('click', logger)
-  }
-  console.log('Клик по кнопке')
-  console.log(counter)
 }
 
 square.addEventListener('click', logger)   // стирает функцию после третьего клика
+
+/*square.addEventListener('mouseup', logger)// отпустить мышку
+square.addEventListener('mousedown', logger)// нажать мышкой
+square.addEventListener('mousemove', logger)// провести курсором по объекту
+*/
+
+square.addEventListener('mouseenter', logger)// функция отрабатывает при входе на объект
+square.addEventListener('mouseleave', logger)// при выходе с объекта 
+
+square.addEventListener('mouseover', logger)// работает  только на самом объекте, если в объекте ещё один объект - переход в него реагирует как на выход с основного объекта, хотщ в нём и находится
+square.addEventListener('mouseout', logger) 
+
+//------------------------------------------------
+
+const text = document.getElementById('text')
+
+
+
+const logger2 = function (event) {
+  console.log(event.type); // описание (type) - типа event - события
+}
+text.addEventListener('input', logger2)
+
+const logger1 = function (event) {  
+  console.dir(event.target);
+ 
+}
+
+const logger3 = function (event) {
+  console.dir(event); // выводит в консоль значение импута
+} 
+
+
+
+text.addEventListener('input',logger1) // отрабатывает при вводе строки один или больше символов
+text.addEventListener('focus', logger1) // отрабатывает при нажатии курсора в импуте
+text.addEventListener('blur', logger1)// отрабатывает при выводе курсора из импута
+text.addEventListener('change', logger1)// отрабатывает при вводе ,выводе курсора из импута и изменения импута
 
